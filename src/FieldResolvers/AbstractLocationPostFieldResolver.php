0a56188317e291a7881e42c4b31426a72331b533
leoloso <leo@getpop.org>
2021-07-08 05:26:52 +0000

    Merge pull request #71 from leoloso/bump-phpstan-level
    Bump PHPStan  to level 5

---
+++ src/FieldResolvers/AbstractLocationPostFieldResolver.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPSchema\LocationPosts\FieldResolvers;
+
+use PoP\ComponentModel\FieldResolvers\AbstractQueryableFieldResolver;
+use PoP\ComponentModel\Schema\SchemaDefinition;
+use PoP\ComponentModel\Schema\SchemaTypeModifiers;
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoPSchema\CustomPosts\Types\Status;
+use PoPSchema\LocationPosts\ComponentConfiguration;
+use PoPSchema\LocationPosts\Facades\LocationPostTypeAPIFacade;
+use PoPSchema\LocationPosts\TypeResolvers\LocationPostTypeResolver;
+use PoPSchema\SchemaCommons\DataLoading\ReturnTypes;
+
+abstract class AbstractLocationPostFieldResolver extends AbstractQueryableFieldResolver
+{
+    public function getFieldNamesToResolve(): array
+    {
+        return [
+            'locationposts',
+        ];
+    }
+
+    public function getSchemaFieldType(TypeResolverInterface $typeResolver, string $fieldName): string
+    {
+        $types = [
+            'locationposts' => SchemaDefinition::TYPE_ID,
+        ];
+        return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldTypeModifiers(TypeResolverInterface $typeResolver, string $fieldName): ?int
+    {
+        return match ($fieldName) {
+            'locationposts' => SchemaTypeModifiers::NON_NULLABLE | SchemaTypeModifiers::IS_ARRAY,
+            default => parent::getSchemaFieldTypeModifiers($typeResolver, $fieldName),
+        };
+    }
+
+    public function getSchemaFieldDescription(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $descriptions = [
+            'locationposts' => $this->translationAPI->__('Location Posts', 'locationposts'),
+        ];
+        return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldArgs(TypeResolverInterface $typeResolver, string $fieldName): array
+    {
+        $schemaFieldArgs = parent::getSchemaFieldArgs($typeResolver, $fieldName);
+        switch ($fieldName) {
+            case 'locationposts':
+                return array_merge(
+                    $schemaFieldArgs,
+                    $this->getFieldArgumentsSchemaDefinitions($typeResolver, $fieldName)
+                );
+        }
+        return $schemaFieldArgs;
+    }
+
+    public function enableOrderedSchemaFieldArgs(TypeResolverInterface $typeResolver, string $fieldName): bool
+    {
+        switch ($fieldName) {
+            case 'locationposts':
+                return false;
+        }
+        return parent::enableOrderedSchemaFieldArgs($typeResolver, $fieldName);
+    }
+
+    /**
+     * @param array<string, mixed> $fieldArgs
+     * @return array<string, mixed>
+     */
+    protected function getQuery(
+        TypeResolverInterface $typeResolver,
+        object $resultItem,
+        string $fieldName,
+        array $fieldArgs = []
+    ): array {
+        switch ($fieldName) {
+            case 'locationposts':
+                return [
+                    'limit' => ComponentConfiguration::getLocationPostListDefaultLimit(),
+                    'status' => [
+                        Status::PUBLISHED,
+                    ],
+                ];
+        }
+        return [];
+    }
+
+    /**
+     * @param array<string, mixed> $fieldArgs
+     * @param array<string, mixed>|null $variables
+     * @param array<string, mixed>|null $expressions
+     * @param array<string, mixed> $options
+     */
+    public function resolveValue(
+        TypeResolverInterface $typeResolver,
+        object $resultItem,
+        string $fieldName,
+        array $fieldArgs = [],
+        ?array $variables = null,
+        ?array $expressions = null,
+        array $options = []
+    ): mixed {
+        $locationPostTypeAPI = LocationPostTypeAPIFacade::getInstance();
+        switch ($fieldName) {
+            case 'locationposts':
+                $query = $this->getQuery($typeResolver, $resultItem, $fieldName, $fieldArgs);
+                $options = [
+                    'return-type' => ReturnTypes::IDS,
+                ];
+                $this->addFilterDataloadQueryArgs($options, $typeResolver, $fieldName, $fieldArgs);
+                return $locationPostTypeAPI->getLocationPosts($query, $options);
+        }
+
+        return parent::resolveValue($typeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
+    }
+
+    public function resolveFieldTypeResolverClass(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        switch ($fieldName) {
+            case 'locationposts':
+                return LocationPostTypeResolver::class;
+        }
+
+        return parent::resolveFieldTypeResolverClass($typeResolver, $fieldName);
+    }
+}
